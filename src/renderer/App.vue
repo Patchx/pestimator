939c9c55afd1770634215ed0d08ca2ada885b66a
Robert Anderson <robert.anderson.fl@gmail.com>
2021-12-13 11:51:15 -0500

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pestimator'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
